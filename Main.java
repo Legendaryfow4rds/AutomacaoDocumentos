f079237fa6e18acb6c0cafbfa79c16f744f39249
Legendaryfow4rds <147113027+Legendaryfow4rds@users.noreply.github.com>
2026-02-18 14:00:40 -0300

    Change main method to call MenuPrincipalFX

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         // Esta chamada indireta contorna a verificação de módulos do JavaFX
         // e permite que o JAR seja executado sem parâmetros complexos de VM.
-        InterfaceAutomacaoFX.main(args);
+        MenuPrincipalFX.main(args);
     }
 }
